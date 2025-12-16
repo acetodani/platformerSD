c1b0cf67cd4aa484255ee804eec05e474410dc52
acetodani <207441415+acetodani@users.noreply.github.com>
2025-12-16 15:33:25 -0600

    window

---
+++ Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Checkers");
+        window.setSize(500, 500);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
+}
